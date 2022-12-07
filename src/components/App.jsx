eb6c8caa86bed423830327c814315e60f3518ab5
AndreyGOIT <ae.borga@gmail.com>
2022-12-07 12:34:50 +0200

    fix in progress

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,38 @@
-import Phonebook from './Phonebook/Phonebook';
+import { Component } from 'react';
+import { ContactsList } from './ContactsList/ContactsList';
+import Form from './Phonebook/Form';
 
-export const App = () => {
-  return (
-    <div>
-      <Phonebook />
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  formSubmitHandler = data => {
+    // console.log(data);
+    this.setState(({ contacts }) => contacts.push(data));
+  };
+
+  render() {
+    const contactsQuantity = this.state.contacts.length;
+    return (
+      <>
+        <Form />
+        {contactsQuantity > 0 ? (
+          <ContactsList contacts={this.state.contacts} />
+        ) : (
+          <span
+            style={{
+              display: 'flex',
+              fontWeight: 'bold',
+              margin: 15,
+            }}
+          >
+            There is no contacts yet
+          </span>
+        )}
+      </>
+    );
+  }
+}
--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,25 +1,43 @@
-import { Component } from 'react';
+// import { Component } from 'react';
+import styles from './ContactsList.module.css';
 
-class ContactsList extends Component {
-  state = {
-    contacts: [],
-    name: '',
-    number: '',
-  };
-  render() {
-    const contacts = this.props.contacts;
+export const ContactsList = ({ contacts }) => {
+  <section className={styles.section}>
+    <p className={styles.sectionTitle}>Contacts</p>
+    <ul>
+      {contacts.map(contact => {
+        return (
+          <li key={contact.id}>
+            {contact.name}: {contact.number}
+          </li>
+        );
+      })}
+    </ul>
+  </section>;
+};
+// class ContactsList extends Component {
+//   state = {
+//     contacts: [],
+//     name: '',
+//     number: '',
+//   };
+//   render() {
+//     const contacts = this.props.contacts;
 
-    return (
-      <ul>
-        {contacts.map(contact => {
-          return (
-            <li key={contact.id}>
-              {contact.name}: {contact.number}
-            </li>
-          );
-        })}
-      </ul>
-    );
-  }
-}
-export default ContactsList;
+//     return (
+//       <section className={styles.section}>
+//         <p className={styles.sectionTitle}>Contacts</p>
+//         <ul>
+//           {contacts.map(contact => {
+//             return (
+//               <li key={contact.id}>
+//                 {contact.name}: {contact.number}
+//               </li>
+//             );
+//           })}
+//         </ul>
+//       </section>
+//     );
+//   }
+// }
+// export default ContactsList;
