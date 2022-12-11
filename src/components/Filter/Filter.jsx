46a1cf29a2848e492fab15a2810a85dda91e547e
AndreyGOIT <ae.borga@gmail.com>
2022-12-11 12:17:49 +0200

    some fix

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Filter.module.css';
 
 const Filter = ({ value, onChange }) => {
   return (
-    <label>
-      <p
-        style={{
-          display: 'flex',
-          margin: 15,
-        }}
-      >
-        Find contacts by name
-      </p>
+    <label className={styles.filterBlock}>
+      <p className={styles.filterTitle}>Find contacts by name</p>
       <input type="text" value={value} onChange={onChange} />
     </label>
   );
